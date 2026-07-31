6c1bf95c055e5673e40deb30f89ea2b32b93af5a
Rishika Patel <134195896+Rishikaaz@users.noreply.github.com>
2026-07-31 07:15:32 +0530

    Time: 15 ms (35.66%) | Memory: 28.3 MB (31.38%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> count(26, 0);
+        for (char c : word) {
+            count[c - 'a']++;
+        }
+        sort(count.rbegin(), count.rend());
+        int totalPushes = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (count[i] == 0) break;
+            int multiplier = (i / 8) + 1;
+            totalPushes += count[i] * multiplier;
+        }
+        
+        return totalPushes;
+    }
+};
