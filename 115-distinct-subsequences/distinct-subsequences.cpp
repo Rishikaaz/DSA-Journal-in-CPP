0115f7716272355af49395133c02bfaee900ae6d
Rishika Patel <134195896+Rishikaaz@users.noreply.github.com>
2026-09-06 08:45:58 +0530

    Time: 8 ms (90.60%) | Memory: 8.9 MB (91.43%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        if (m > n)
+            return 0;
+        vector<unsigned long long> dp(m + 1, 0);
+        dp[0] = 1;
+        for (char c : s) {
+            for (int j = m - 1; j >= 0; --j) {
+                if (c == t[j]) {
+                    dp[j + 1] += dp[j];
+                }
+            }
+        }
+        return static_cast<int>(dp[m]);
+    }
+};
