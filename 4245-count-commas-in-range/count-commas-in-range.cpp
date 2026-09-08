16106c0abcc76dcf8d9e03a75b7d487231faa68e
Rishika Patel <134195896+Rishikaaz@users.noreply.github.com>
2026-09-08 09:19:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.38%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int cnt = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i >= 1000)
-                cnt++;
-        }
-        return cnt;
+        if (n < 1000)
+            return 0;
+
+        return (n - 1000) + 1;
     }
 };
